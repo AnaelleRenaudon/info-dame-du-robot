dff114912186f48e94507bb22f25c1cba5b04be3
anaelle <anaelle.renaudon@outlook.fr>
2025-11-03 15:33:33 +0100

    999

--- exec9.c
+++ exec9.c
@@ -3,6 +3,8 @@
 int main() {
     int a = 10;
     int b = 20;
-    printf("a=%d b=%d\n", b); 
+    printf("a=%d b=%d\n", a, b); 
     return 0;
 }
+
+/* On a deux %d donc il y en a un qui ne correspond à rien : il faut que chaque %d corresponde à un argument de la chaine */
