9fe512d0858b682f53fe53abecb872ee1c264240
anaelle <anaelle.renaudon@outlook.fr>
2025-11-03 14:48:54 +0100

    stu

--- compil11.c
+++ compil11.c
@@ -10,4 +10,4 @@ int main() {
 }
 
 /*compil11.c:9:5: error: expected identifier or '(' before 'return' return 0;*compil11.c:10:1: error: expected identifier or '(' before '}' token
- }il manque une ouverture d'accolade */
+ }il manque une ouverture d'accolade après le else ligne 6  */
