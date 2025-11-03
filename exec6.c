969a6cbbca3ab5669d5f6d042fe392b96224c0c9
anaelle <anaelle.renaudon@outlook.fr>
2025-11-03 15:19:17 +0100

    ex6

--- exec6.c
+++ exec6.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    char nom[4];
+    char nom[50];
     printf("Nom ? ");
     scanf("%s", nom); 
     printf("Bonjour %s\n", nom);
     return 0;
 }
+/*la taille du tableau est trop petite ( seulement 3 caracteres car il y a le caractere nul à la fin) donc j'ai juste augmenter la taille du tableau */
