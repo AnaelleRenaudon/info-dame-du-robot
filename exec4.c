ca771125e25c99ac34d97eae622357dd091672f9
anaelle <anaelle.renaudon@outlook.fr>
2025-11-03 15:13:30 +0100

    hh

--- exec4.c
+++ exec4.c
@@ -4,6 +4,10 @@ int main() {
     int i = 0;
     while (i < 10) {
         printf("%d\n", i);
+        i++; 
     }
     return 0;
 }
+
+/* i n’est jamais modifiée dans la boucle while donc il faut incrémenter i sinon la condition est toujours vraie
+*/
