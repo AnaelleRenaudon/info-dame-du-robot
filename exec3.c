3aae9adb66e08c8159fee8e087e450b1e44d78f4
anaelle <anaelle.renaudon@outlook.fr>
2025-11-03 15:08:46 +0100

    hh

--- exec3.c
+++ exec3.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int x;
-    int y = x + 5;  
-    printf("%d\n", y);
+    int x = 10;          
+    int y = x + 5;
+    printf("%d\n", y); 
     return 0;
 }
+/*la variable x est déclarée mais pas initialisée avant qu'on l'utilise dans y = x + 5 */
