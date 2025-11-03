37d0938616695838b336cf82d0cfce4cde484d1c
anaelle <anaelle.renaudon@outlook.fr>
2025-11-03 15:16:05 +0100

    55

--- exec5.c
+++ exec5.c
@@ -6,6 +6,8 @@ int main() {
     s[1] = 'e';
     s[2] = 'y';
     s[3] = '!';
-    printf("%s\n", s); 
+    s[4] = '\0'; 
+    printf("%s\n", s);
     return 0;
 }
+/* il n'y avait pas de caractère nul à la fin : \o donc le programme peut planter ou ne pas afficher les bons caracteres */
