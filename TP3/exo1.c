8a45592ddef5ed555cef683a21482b689abf0dcc
anaelle <anaelle.renaudon@outlook.fr>
2025-11-17 15:15:37 +0100

    etape4

--- TP3/exo1.c
+++ TP3/exo1.c
@@ -3,11 +3,12 @@
 
 int main() {
     int n;
+
     printf("Entrez une taille pour l'etoile entre 5 et 10 : ");
     scanf("%d", &n);
 
     while (n < 5 || n > 10) {
-        printf("Valeur incorrecte, entrez une taille entre 5 et 10 : ");
+        printf("Valeur incorrecte,entrez une taille entre 5 et 10 : ");
         scanf("%d", &n);
     }
 
@@ -16,36 +17,38 @@ int main() {
 
     char img[max_height][width];
 
-
     for (int i = 0; i < max_height; i++) {
         for (int j = 0; j < width; j++) {
             img[i][j] = ' ';
         }
     }
 
-    
-    for (int i = 0; i < max_height; i++) {
-        for (int j = 0; j < width; j++) {
-            printf("%c", img[i][j]);
-        }
-        printf("\n");
-    }
-
-    
     int mid = width / 2;
 
-   
     for (int i = 0; i < n; i++) {
+        int left = mid - i;
+        int right = mid + i;
+
         for (int j = 0; j < width; j++) {
-            if (j == mid - i || j == mid + i) {
-                img[i][j] = 'A';
-            } else if (j > mid - i && j < mid + i) {
-                img[i][j] = 'S';
+
+            if (i < n - 1) {
+                if (j == left || j == right)
+                    img[i][j] = 'A';
+                else if (j > left && j < right)
+                    img[i][j] = 'S';
             }
 
+            else {
+                img[i][j] = '-';
+
+                if (j == left || j == right)
+                    img[i][j] = 'A';
+                else if (j > left && j < right)
+                    img[i][j] = 'S';
+            }
         }
     }
- 
+
     for (int i = 0; i < max_height; i++) {
         for (int j = 0; j < width; j++) {
             printf("%c", img[i][j]);
@@ -55,5 +58,6 @@ int main() {
 
     return 0;
 }
+
 
 
