d56f26161375f8c251c7fdbf9c45e31b1ed86af9
anaelle <anaelle.renaudon@outlook.fr>
2025-11-24 15:29:02 +0100

    part3

--- TP4/exo2.c
+++ TP4/exo2.c
@@ -8,8 +8,27 @@ void afficher_menu() {
 
 int saisir_montant() {
     int montant;
-    printf("Entrez un montant : ");
+    printf("\nEntrez un montant : \n");
     scanf("%d", &montant);
     return montant;
 }
 
+int montant_valide(int montant) {
+
+    if (montant <= 0) {
+        printf("Erreur : le montant doit etre positif.\n");
+        return 0;
+    }
+
+    if (montant > 1000) {
+        printf("Erreur : le montant maximum est 1000€.\n");
+        return 0;
+    }
+
+    if (montant % 5 != 0) {
+        printf("Erreur : le montant doit etre un multiple de 5.\n");
+        return 0;
+    }
+
+    return 1;
+}
