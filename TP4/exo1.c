d8d7a006d18b9857848f6311c7aa2cf99e0a10bd
anaelle <anaelle.renaudon@outlook.fr>
2025-11-24 13:08:11 +0100

    partie1

---
+++ TP4/exo1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int afficherMenuNotes() {
+    printf("\n--- MENU NOTES ---\n");
+    printf("1. Saisir nombre d'eleves\n");
+    printf("2. Saisir notes\n");
+    printf("3. Afficher notes\n");
+    printf("4. Moyenne d'un eleve\n");
+    printf("5. Moyenne generale\n");
+    printf("6. Meilleures notes\n");
+    printf("7. Retour\n");
+    return 0;
+}
+
