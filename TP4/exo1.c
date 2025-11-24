576446b3f07fff2ed70c656cbecdc760188c04f0
anaelle <anaelle.renaudon@outlook.fr>
2025-11-24 13:19:40 +0100

    etape2,3

--- TP4/exo1.c
+++ TP4/exo1.c
@@ -13,3 +13,28 @@ int afficherMenuNotes() {
     return 0;
 }
 
+int lireChoix() {
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+
+int saisirNombreEleves() {
+    int n;
+     {
+        printf("Saisissez un nombre d'eleves entre 1 et 30  ");
+        scanf("%d", &n);
+    } while (n < 1 || n > 30);
+    return n;
+}
+
+int saisirNotes(float t[][3], int nb) {
+    for (int i = 0; i < nb; i++)
+        for (int j = 0; j < 3; j++) {
+             {
+                printf("Eleve %d C%d : ", i + 1, j + 1);
+                scanf("%f", &t[i][j]);
+            } while (t[i][j] < 0 || t[i][j] > 20);
+        }
+    return 0;
+}
