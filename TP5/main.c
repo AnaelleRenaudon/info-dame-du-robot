beb78be86bafe51e7f0ad2457a937dcfcacef55e
anaelle <anaelle.renaudon@outlook.fr>
2025-12-01 13:57:18 +0100

    etape1finie

---
+++ TP5/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "TP5.h"
+
+int main() {
+    int conso[7]; 
+    int choix;
+
+    afficherMenu();
+    choix = lireChoixUtilisateur();
+    
+    while (choix != 4) {
+        switch (choix) {
+            case 1:
+            case 2:
+            case 3:
+                break;
+            case 4:
+                break; 
+        }
+
+        if (choix < 1 || choix > 4) {
+             printf("Choix invalide. Veuillez reessayer.\n");
+        }
+
+        afficherMenu();
+        choix = lireChoixUtilisateur();
+    }
+    
+    printf("Arret du programme. Au revoir !\n");
+
+    return 0;
+}
