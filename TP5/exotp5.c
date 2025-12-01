40dbe4fb66d1cb49fc669a96d9ecd1b800ac71b1
anaelle <anaelle.renaudon@outlook.fr>
2025-12-01 13:49:47 +0100

    2

--- TP5/exotp5.c
+++ TP5/exotp5.c
@@ -5,8 +5,7 @@ void afficherMenu() {
     printf("\n============ Suivi de consommation ============\n");
     printf("1. Ajouter une consommation\n");            
     printf("2. Afficher le resume du jour\n");           
-    printf("3. Afficher les objectifs et le score\n");   
-    printf("4. Sauvegarder et quitter\n");               
+    printf("3. Sauvegarder et quitter\n");               
     printf("Votre choix : ");                           
 }
 
