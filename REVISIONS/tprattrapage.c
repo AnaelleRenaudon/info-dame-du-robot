6d49639362a5e0948cf551874e8f9f71636e0bac
anaelle <anaelle.renaudon@outlook.fr>
2026-02-16 14:30:41 +0100

    fin de saisie des scores

--- REVISIONS/tprattrapage.c
+++ REVISIONS/tprattrapage.c
@@ -14,16 +14,31 @@ char affichageMatch (){
 
 }
 
-char saisieScore (int nbmatch , int indice, char nom[];) {
+char saisieScore (int nbmatch , int indice, char nom[],float score_joueur1,float score_joueur2,) {
+  
+  printf("Entrez le score du joueur 1 ( Jean ou Paul ) :");
+  scanf("%d",&score_joueur1);
+  printf("Entrez le score du joueur 2 ( Alban ou Jules ) :");
+  scanf("%d",&score_joueur2);
+  
+  if(score_joueur1=score_joueur2){
 
-   
+    printf("Egalité.\n");
+    return 0;}
+
+  else if (score_joueur1 > score_joueur2){
+
+    /*Je n'arrivais pas à faire renvoyer les noms des joueurs donc jai mis leur indice directement */
+
+    printf("Félicitations , le joueur 1 gagne contre le joueur 2 %d - %d\n" , score_joueur1 , score_joueur2 );
+}
+
+  else if (score_joueur1 < score_joueur2) {
+    printf("Félicitations , le joueur 2 gagne contre le joueur 1 %d - %d\n" , score_joueur2 , score_joueur1);
+    }
 }
 
 int main () {
-
-char nom[];
-int age;
-int score;
 
 
 
