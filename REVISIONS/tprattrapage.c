560d2eb91470adba314022cdece2fc831b020284
anaelle <anaelle.renaudon@outlook.fr>
2026-02-16 14:09:56 +0100

    fonction presques finies

--- REVISIONS/tprattrapage.c
+++ REVISIONS/tprattrapage.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 
+void inscriptionJoueurs () {
+    int nbjoueurs = 4;
+    char nom[];
+    int age[];
+
+}
+
+char affichageMatch (){
+    char nom[];
+    int nbjoueurs = 4;
+    int indice; 
+
+}
+
+char saisieScore (int nbmatch , int indice, char nom[];) {
+
+   
+}
+
 int main () {
 
 char nom[];
 int age;
 int score;
-int joueur1 ;
-int joueur2 ;
+
 
 
 
